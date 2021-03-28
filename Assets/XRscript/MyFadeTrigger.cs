9d239885e3acda25b325d1fae4aadc07c50456e8
LeeJohnn <bello44691@gmail.com>
2021-03-29 03:13:23 +0800

    Attach some scripts to Scene 5

---
+++ Assets/XRscript/MyFadeTrigger.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyFadeTrigger : MonoBehaviour
+{
+    public bool canTrigger;
+    public MyFadeOutController fadeOutController;
+
+    void OnTriggerStay(Collider other)
+    {
+        if(canTrigger && other.tag.Equals("Hand")){
+            canTrigger = false;
+            fadeOutController.isStart = true;
+          
+    }
+    }
+}
---
+++ Assets/XRscript/MySubTrigger0.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MySubTrigger0 : MonoBehaviour
+{
+    public bool canTrigger;
+    public MySubtiteController subtiteController;
+
+    void OnTriggerStay(Collider other)
+    {
+        if(canTrigger && other.tag.Equals("Hand")){
+            canTrigger = false;
+            subtiteController.isStart = true;
+          
+    }
+    }
+}
