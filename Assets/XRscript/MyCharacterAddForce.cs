c2fe03e72f10c4f264348bb76c6e0047508fe4d0
LeeJohnn <bello44691@gmail.com>
2021-03-29 22:17:39 +0800

    Attach some scripts to scene6

---
+++ Assets/XRscript/MyCharacterAddForce.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyCharacterAddForce : MonoBehaviour
+{
+    public bool IsAddForce;
+    public Vector3 forceMag;
+
+    private Rigidbody rb;
+
+    public MySubtiteController mySubtiteController;
+
+    public MySubtiteController mySubtiteControllerInWater;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        mySubtiteController.EndTalkHandler += AddForce;
+        
+    }
+    
+      void Update()
+    {
+        if( IsAddForce){
+             IsAddForce = false;
+             AddForce();
+        }
+    }
+
+    // Update is called once per frame
+    public void AddForce(){
+        rb.AddForce(forceMag);
+        mySubtiteControllerInWater.isStart = true;
+    }
+
+    public void AdjustDrag(){
+        rb.drag = 8;
+    }
+}
