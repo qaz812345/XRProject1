6777d2e6c82b5f38efeb10e2928e022b5cb541e6
LeeJohnn <bello44691@gmail.com>
2021-03-31 22:35:30 +0800

    build test

---
+++ Assets/MyGravityRunTime.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyGravityRunTime : MonoBehaviour
+{
+    public Vector3 gravityThisScene;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Physics.gravity = gravityThisScene;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
